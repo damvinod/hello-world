bf76780c147d20a0bf6b5617e24c28b98e51adf0
Vinod Dampuru <vinodreddyreddy25@gmail.com>
2025-01-10 12:10:36 +0800

    feat: Adding loggers

--- src/main/java/com/damvinod/hello/world/HelloWorldController.java
+++ src/main/java/com/damvinod/hello/world/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.damvinod.hello.world;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.http.ResponseEntity.status;
 
 @RestController
+@Slf4j
 public class HelloWorldController {
 
     @GetMapping(path = "/hello")
     public ResponseEntity<String> helloWorld() {
+        log.info("Hello world Service controller invoked.");
         return status(HttpStatus.OK).body("Hello");
     }
 }
